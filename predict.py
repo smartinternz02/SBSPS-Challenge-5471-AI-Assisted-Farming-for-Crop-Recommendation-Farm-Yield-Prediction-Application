25923321f0b0781ed2b27fcf6b12734f8dc23c28
kaajalkrishnamurthy <78948609+kaajalkrishnamurthy@users.noreply.github.com>
2021-08-31 23:04:39 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,9 +1,14 @@
+import re
 from flask import Flask,render_template,request
 
 app=Flask(__name__)
 @app.route('/crop_prediction')
 def home():
     return render_template('home.html')
+
+@app.route('/output')
+def output():
+    return render_template('output.html')
     
 if __name__=="__main__":
     app.run()
